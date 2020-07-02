1731fbc31e58a26b4d597a5db4856e174a168365
aram ju <aaadkfka@gmail.com>
2020-07-02 19:34:07 +0900

    sample list

--- App.tsx
+++ App.tsx
@@ -15,19 +15,22 @@ import {
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
+  Image,
+  Button,
+  Alert,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import tailwind from 'tailwind-rn';
+// import { t } from 'react-native-tailwindcss';
+// import styled from 'styled-components/native';
 
 declare const global: {HermesInternal: null | {}};
 
+// const SView = styled.View`
+//   ${[t.mB4, t.bgGray200, t.rounded, t.p3]}
+// `;
 const App = () => {
   return (
     <>
@@ -35,40 +38,144 @@ const App = () => {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
+          style={tailwind('p-4')}>
+          {/* <SView>
+            <Text>Some unstyled text in a styled view</Text>
+          </SView> */}
+          <View style={tailwind('flex flex-row bg-gray-200 py-2 px-4 mb-4')}>
+            <TextInput
+              placeholder="What are you looking for?"
+              style={tailwind('text-base')}
+            />
+          </View>
+
+          <View style={tailwind('w-full mb-4')}>
+            <Image
+              style={tailwind('w-full h-40')}
+              source={{
+                uri:
+                  'https://chblobdev.icloudhospital.com/thumbnailcontainer/0b59a18f-db28-4754-89b4-af7bca99ab81-2988ffd6-d89b-4eb1-88da-eb42ce06188e.jpg'
+              }}
+            />
+            <Text style={tailwind('px-6 pt-2 text-lg font-semibold')}>
+              Dental Clinic
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              Dentistry, Cosmetology
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              3.2km (maps)
+            </Text>
+            <View style={tailwind('px-6 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Next Available:
               </Text>
+              <Text style={tailwind('text-sm text-red-700')}>Tomorrow</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
+            <View style={tailwind('px-6 pb-4 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Consultation fees:
               </Text>
+              <Text style={tailwind('text-sm text-red-700')}>~ $30</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
+
+            <View style={tailwind('w-full flex flex-row bg-gray-200')}>
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="info"
+                color="#0F4C81"
+              />
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="Online Booking"
+              />
+            </View>
+          </View>
+
+          <View style={tailwind('w-full mb-4')}>
+            <Image
+              style={tailwind('w-full h-40')}
+              source={{
+                uri:
+                  'https://chblobdev.icloudhospital.com/thumbnailcontainer/0b59a18f-db28-4754-89b4-af7bca99ab81-2988ffd6-d89b-4eb1-88da-eb42ce06188e.jpg'
+              }}
+            />
+            <Text style={tailwind('px-6 pt-2 text-lg font-semibold')}>
+              Dental Clinic
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              Dentistry, Cosmetology
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              3.2km (maps)
+            </Text>
+            <View style={tailwind('px-6 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Next Available:
               </Text>
+              <Text style={tailwind('text-sm text-red-700')}>Tomorrow</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+            <View style={tailwind('px-6 pb-4 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Consultation fees:
               </Text>
+              <Text style={tailwind('text-sm text-red-700')}>~ $30</Text>
             </View>
-            <LearnMoreLinks />
+
+            <View style={tailwind('w-full flex flex-row bg-gray-200')}>
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="info"
+                color="#0F4C81"
+              />
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="Online Booking"
+              />
+            </View>
+          </View>
+
+          <View style={tailwind('w-full mb-4')}>
+            <Image
+              style={tailwind('w-full h-40')}
+              source={{
+                uri:
+                  'https://chblobdev.icloudhospital.com/thumbnailcontainer/0b59a18f-db28-4754-89b4-af7bca99ab81-2988ffd6-d89b-4eb1-88da-eb42ce06188e.jpg'
+              }}
+            />
+            <Text style={tailwind('px-6 pt-2 text-lg font-semibold')}>
+              Dental Clinic
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              Dentistry, Cosmetology
+            </Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>
+              3.2km (maps)
+            </Text>
+            <View style={tailwind('px-6 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Next Available:
+              </Text>
+              <Text style={tailwind('text-sm text-red-700')}>Tomorrow</Text>
+            </View>
+            <View style={tailwind('px-6 pb-4 flex flex-row')}>
+              <Text style={tailwind('text-sm text-gray-700')}>
+                Consultation fees:
+              </Text>
+              <Text style={tailwind('text-sm text-red-700')}>~ $30</Text>
+            </View>
+
+            <View style={tailwind('w-full flex flex-row bg-gray-200')}>
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="info"
+                color="#0F4C81"
+              />
+              <Button
+                onPress={() => Alert.alert('Left button pressed')}
+                title="Online Booking"
+              />
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -77,42 +184,42 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  // scrollView: {
+  //   backgroundColor: Colors.lighter,
+  // },
+  // engine: {
+  //   position: 'absolute',
+  //   right: 0,
+  // },
+  // body: {
+  //   backgroundColor: Colors.white,
+  // },
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  //   color: Colors.black,
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  //   color: Colors.dark,
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
 });
 
 export default App;
