8858ab8ca5bff91bbfa20557c7b323d2b344c9fc
aram ju <aaadkfka@gmail.com>
2020-07-07 11:47:47 +0900

    tailwind-rn customize

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
 };
--- App.tsx
+++ App.tsx
@@ -8,30 +8,20 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  StatusBar,
-  Image,
-  Button,
-  Alert,
-} from 'react-native';
+import React from 'react'
+import { SafeAreaView, ScrollView, View, Text, TextInput, StatusBar, Image, Button, Alert } from 'react-native'
 
-import tailwind from 'tailwind-rn';
-import styled from 'styled-components/native';
-import {t} from 'react-native-tailwindcss';
+import { create } from 'tailwind-rn'
+import styles from './styles.json'
+
+const { tailwind } = create(styles)
+import styled from 'styled-components/native'
+import { t } from 'react-native-tailwindcss'
 
 const App = () => {
-  const Card = styled.View`
-    ${[t.mB4, t.bgWhite, t.shadowLg, t.roundedLg]}
-  `;
   const ImageView = styled.View`
     ${[t.roundedTlLg, t.roundedTrLg, t.overflowHidden]}
-  `;
+  `
   const ButtonView = styled.View`
     ${[
       t.h10,
@@ -50,17 +40,12 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={tailwind('p-4')}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={tailwind('p-4')}>
           <View style={tailwind('flex flex-row bg-gray-200 py-2 px-4 mb-4')}>
-            <TextInput
-              placeholder="What are you looking for?"
-              style={tailwind('text-base')}
-            />
+            <TextInput placeholder="What are you looking for?" style={tailwind('text-base')} />
           </View>
 
-          <Card>
+          <View style={tailwind('mb-4 bg-white rounded-lg shadow-lg')}>
             <ImageView>
               <Image
                 style={tailwind('w-full h-40')}
@@ -70,44 +55,27 @@ const App = () => {
                 }}
               />
             </ImageView>
-            <Text style={tailwind('px-6 pt-2 text-lg font-semibold')}>
-              Dental Clinic
-            </Text>
-            <Text style={tailwind('px-6 text-sm text-gray-700')}>
-              Dentistry, Cosmetology
-            </Text>
-            <Text style={tailwind('px-6 text-sm text-gray-700')}>
-              3.2km (maps)
-            </Text>
+            <Text style={tailwind('px-6 pt-2 text-lg font-semibold')}>Dental Clinic</Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>Dentistry, Cosmetology</Text>
+            <Text style={tailwind('px-6 text-sm text-gray-700')}>3.2km (maps)</Text>
             <View style={tailwind('px-6 flex flex-row')}>
-              <Text style={tailwind('text-sm text-gray-700')}>
-                Next Available:
-              </Text>
+              <Text style={tailwind('text-sm text-gray-700')}>Next Available:</Text>
               <Text style={tailwind('text-sm text-red-700')}>Tomorrow</Text>
             </View>
             <View style={tailwind('px-6 pb-4 flex flex-row')}>
-              <Text style={tailwind('text-sm text-gray-700')}>
-                Consultation fees:
-              </Text>
+              <Text style={tailwind('text-sm text-gray-700')}>Consultation fees:</Text>
               <Text style={tailwind('text-sm text-red-700')}>~ $30</Text>
             </View>
 
             <ButtonView>
-              <Button
-                onPress={() => Alert.alert('Left button pressed')}
-                title="info"
-                color="#0F4C81"
-              />
-              <Button
-                onPress={() => Alert.alert('Left button pressed')}
-                title="Online Booking"
-              />
+              <Button onPress={() => Alert.alert('Left button pressed')} title="info" color="#0F4C81" />
+              <Button onPress={() => Alert.alert('Left button pressed')} title="Online Booking" />
             </ButtonView>
-          </Card>
+          </View>
         </ScrollView>
       </SafeAreaView>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
